667436778dcbff5c05400335d49c40e612ac00e2
mul53 <mulengabowa53@gmail.com>
2025-06-12 17:47:01 +0200

    add fixes to find agents tasks

--- src/services/ensemble.ts
+++ src/services/ensemble.ts
@@ -2,29 +2,44 @@ import { findAgentsByDescription, findAgentsByService } from "../utils/graphql/q
 
 export async function findAgents(searchTerm: string) {
     const matchedAgents = [] 
+    const matchedAgentIds = new Set<string>()
     
     const agentsByService = await findAgentsByService(searchTerm);
 
-    agentsByService.proposals.forEach((agent: any) => {
+    agentsByService.proposals.forEach((agent) => {
+        if (matchedAgentIds.has(agent.issuer.id)) {
+            return
+        }
+        
         matchedAgents.push({
             id: agent.issuer.id,
             name: agent.issuer.metadata.name,
             description: agent.issuer.metadata.description,
+            imageUri: agent.issuer.metadata.imageUri,
             agentUri: agent.issuer.agentUri,
             reputation: agent.issuer.reputation,
         })
+
+        matchedAgentIds.add(agent.issuer.id)
     });
 
     const agentsByDescription = await findAgentsByDescription(searchTerm);
 
-    agentsByDescription.agents.forEach((agent: any) => {
+    agentsByDescription.agents.forEach((agent) => {
+        if (matchedAgentIds.has(agent.id)) {
+            return
+        }
+
         matchedAgents.push({
             id: agent.id,
             name: agent.name,
-            description: agent.description,
+            description: agent.metadata.description,
+            imageUri: agent.metadata.imageUri,
             agentUri: agent.agentUri,
             reputation: agent.reputation,
         })
+
+        matchedAgentIds.add(agent.id)
     });
 
     return matchedAgents;
