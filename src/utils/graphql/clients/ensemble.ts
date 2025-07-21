f73529b6f8cb85b4a31407c6e1519fa786bfbb54
mul53 <mulengabowa53@gmail.com>
2025-07-21 12:43:37 +0200

    remvove hardcoded subgraph url

--- src/utils/graphql/clients/ensemble.ts
+++ src/utils/graphql/clients/ensemble.ts
@@ -5,5 +5,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const ensembleClient = new GraphQLClient(
-    process.env.ENSEMBLE_SUBGRAPH_URL || 'https://api.goldsky.com/api/public/project_cmcnps2k01akp01uobifl4bby/subgraphs/ensemble-subgraph/0.0.1/gn',
+    process.env.ENSEMBLE_SUBGRAPH_URL,
 )
